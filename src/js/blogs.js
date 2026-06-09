440ea39fc5a7355562abc2f3c997b4f80b36d092
Dalton Bowling <dbowl456@gmail.com>
2026-06-08 23:27:45 -0400

    Added a blog post and make sure it is aligned right

--- src/js/blogs.js
+++ src/js/blogs.js
@@ -2,5 +2,9 @@ const blogs = {
     "2026-06-06": {
         title: "My first post",
         body: "Hello everyone! This is my first blog post. As the description says, it's just a place I can talk about random things. It could be able Computer Science or not, it truly will be random, but I think I'll enjoy this and I'll try to keep this up! I know this page isn't styled yet (if you are seeing it in June... but I'm working on it!)"
+    },
+    "2026-06-08": {
+        title: "Getting back into Fortnite",
+        body: "I know this is super random, but my friends and I have been getting back into Fortnite. I played it basically since it came out. I started playing in Chapter 1 Season 3, when my brother gave me a playstation when he moved to pc gaming. However, I stopped playing when I got to college because college is BUSY! I basically have no time to do stuff while I'm in college, but now that I'm back at home I just started playing and it's been a lot of fun. I've been playing with some friends and that's been a blast too. I know this is a random post, but this will continue to just be an outlet where I can ramble on about random stuff. Thanks for reading :) "
     }
 };
