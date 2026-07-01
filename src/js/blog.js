66ac5596bd2a9487a5feac6cf5c7caa10b48f2f8
Dalton Bowling <dbowl456@gmail.com>
2026-07-01 01:25:35 -0400

    Added some small stuff to blog, and added more stuff to portfolio

--- src/js/blog.js
+++ src/js/blog.js
@@ -1,9 +1,10 @@
+import { blogs } from "./blogs.js";
+
 const datePicker = document.getElementById("blog-date");
 
 const now = new Date()
 const today = `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, '0')}-${String(now.getDate()).padStart(2, '0')}`
 datePicker.value = today;
-console.log(today);
 
 function loadBlogPost(date) {
     const blogBottom = document.querySelector(".blog-bottom");
@@ -15,12 +16,19 @@ function loadBlogPost(date) {
         const title = document.createElement("h2");
         title.textContent = post.title;
         title.className = "blog-heading";
+        blogBottom.appendChild(title);
+
+        if (post.image) {
+            const img = document.createElement("img");
+            img.src = post.image;
+            img.alt = post.imageAlt;
+            img.className = "blog-image";
+            blogBottom.appendChild(img);
+        }
 
         const text = document.createElement("p");
         text.textContent = post.body;
         text.className = "blog-text";
-
-        blogBottom.appendChild(title);
         blogBottom.appendChild(text);
     } else {
         blogBottom.innerHTML = "<span class=blog-text>No blog post found for that date.</span>"
--- src/js/blogs.js
+++ src/js/blogs.js
@@ -1,4 +1,6 @@
-const blogs = {
+const gbaImg = new URL("../images/blog_images/gba.jpg", import.meta.url).href;
+
+export const blogs = {
     "2026-06-06": {
         title: "My first post",
         body: "Hello everyone! This is my first blog post. As the description says, it's just a place I can talk about random things. It could be able Computer Science or not, it truly will be random, but I think I'll enjoy this and I'll try to keep this up! I know this page isn't styled yet (if you are seeing it in June... but I'm working on it!)"
@@ -10,5 +12,11 @@ const blogs = {
     "2026-06-14": {
         title: "Birthday Weekend!",
         body: "My birthday was on June 10th and it was the good ol' 21. What a week it has been. My birthday was great, we went out to eat and I got some cool things. Now one of the coolest things that I got for my birthday was a GameCube. It was on a whim, but I had some birthday cash and before my birthday I kept eyeing up a GameCube, but I never pulled the trigger. The day after my birthday I went to my local retro video game store, Halcyon Videogames. Along with the GameCube I picked up Super Mario Sunshine and this has been a blast so far! I know there are ways to play it on the switch or just emulate it, but playing on the actually hardware is amazing. Especially if I get games like Double Dash and the Mario Party games it will be a lot easier to play on authentic hardware, but it doesn't stop there. On my way back to my girlfriend's house, she was scrolling on Facebook, and she saw that a GameBoy Advanced was advertised from our local thrift store for 15 bucks. 15 BUCKS. So we went straight over there and I bought it and it WORKS. IT WORKS. It's been an insane birthday week and now my love for retro video games has gone even stronger and the hunt for more games and consoles begin! Thanks for reading :)"
-    }
+    },
+    "2026-06-30": {
+        title: "Modding my GBA",
+        body: "Testing",
+        image: gbaImg,
+        imageAlt: "A Modded GameBoy Advance"
+    },
 };
